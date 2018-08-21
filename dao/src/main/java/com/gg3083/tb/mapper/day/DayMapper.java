af38841d84e8686019cf0c27a0b63f9110c28c9f
ggNo1 <g@1998.ws>
2018-08-21 22:24:13 +0800

    JUIT

---
+++ dao/src/main/java/com/gg3083/tb/mapper/day/DayMapper.java
@@ -0,0 +1,8 @@
+package com.gg3083.tb.mapper.day;
+
+import com.gg3083.tb.common.mapper.BaseMapper;
+import com.gg3083.tb.domain.day.Day;
+import com.gg3083.tb.domain.userrole.UserRole;
+
+public interface DayMapper extends BaseMapper<Day> {
+}
