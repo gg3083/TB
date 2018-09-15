1256c1369105162041f0d346475b8f3640e27210
ggNo1 <g@1998.ws>
2018-09-15 16:21:17 +0800

    callback

--- web/src/main/java/cn/gg3083/tb/Application.java
+++ web/src/main/java/cn/gg3083/tb/Application.java
@@ -7,7 +7,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 
 @MapperScan({"cn.gg3083.tb.controller","cn.gg3083.tb.mapper"})
 @ComponentScan(basePackages = {"cn.gg3083.tb.controller","cn.gg3083.tb.service","cn.gg3083.tb.mapper","cn.gg3083.tb.bean"})
-@SpringBootApplication()
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ web/src/main/java/cn/gg3083/tb/controller/CallBackController.java
@@ -0,0 +1,29 @@
+package cn.gg3083.tb.controller;
+
+import cn.gg3083.tb.json.JsonResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+@RestController
+public class CallBackController {
+
+    @RequestMapping("callback")
+    public JsonResult random(){
+        JsonResult jsonResult = new JsonResult();
+        List<Integer> list = new LinkedList<>();
+        Random num =   new Random( );
+        for (int i = 0; i < 6; i++) {
+         list.add( num.nextInt(32)+1 );
+        }
+        Collections.sort( list );
+        list.add( num.nextInt(16)+1 );
+        jsonResult.setObj( list );
+        return jsonResult;
+    }
+
+}
